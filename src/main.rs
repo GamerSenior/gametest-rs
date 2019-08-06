2129a97b8899fcb2ccdcc2a5edd16e497cf32769
Giovani Garcia <giovani.abel@supportweb.com.br>
2019-08-06 17:50:06 -0300

    Initialized SDl2 context

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,27 @@ extern crate specs_derive;
 
 use specs::prelude::*;
 use std::{ thread, time::Duration };
+use sdl2::event::Event;
+use sdl2::image::{self, LoadTexture, InitFlag};
 
 mod systems;
 use systems::{ MovimentSystem, Position };
 
-fn main() {
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
+
+    let window = video_subsystem.window("Testing Window", 800, 600)
+        .position_centered()
+        .build()
+        .expect("Não foi possível inicializar os subsistemas de vídeo");
+
+    let mut canvas = window.into_canvas().build()
+        .expect("Não foi possível criar o canvas");
+    let texture_creator = canvas.texture_creator();
+
     let mut world = World::new();
     let mut dispatcher = DispatcherBuilder::new().with(MovimentSystem, "sys_mov", &[]).build();
     dispatcher.setup(&mut world.res);
@@ -20,4 +36,6 @@ fn main() {
     // read_synchronously();
     println!("Testing...");
     thread::sleep(Duration::from_millis(2000));
+
+    Ok(())
 }
